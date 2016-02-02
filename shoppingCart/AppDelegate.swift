1c2ee4f465a9f3e4c9e003e23c1813e5364d3a60
Arslan Ablikim <arslan.ablikim0@gmail.com>
2016-02-02 10:07:50 +0800

    Initial Commit

---
+++ shoppingCart/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  shoppingCart
+//
+//  Created by ئ‍ارسلان ئابلىكىم on 2/2/16.
+//  Copyright © 2016 ئ‍ارسان ئابلىكىم. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ shoppingCart/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  shoppingCart
+//
+//  Created by ئ‍ارسلان ئابلىكىم on 2/2/16.
+//  Copyright © 2016 ئ‍ارسان ئابلىكىم. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
